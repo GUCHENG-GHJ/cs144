6038a90ed36f92a78c428c1fb65f92d09e2fc160
Ubuntu <jake@jake.1ybrdcc4fpkehdprmwxn5rbqcc.hx.internal.cloudapp.net>
2023-06-02 16:43:45 +0000

    lab0

--- apps/webget.cc
+++ apps/webget.cc
@@ -9,8 +9,21 @@ using namespace std;
 
 void get_URL( const string& host, const string& path )
 {
-  cerr << "Function called: get_URL(" << host << ", " << path << ")\n";
-  cerr << "Warning: get_URL() has not been implemented yet.\n";
+  TCPSocket tcp;
+  tcp.connect(Address(host, "http"));
+  tcp.write(
+        "GET "   + path + " HTTP/1.1\r\n" +
+        "Host: " + host + "\r\n" +
+        "Connection: close\r\n\r\n"
+    );
+  std::string str;
+  while(!tcp.eof()) {
+    tcp.read(str);
+    cout << str << endl;
+  }
+  tcp.close();
+  // cerr << "Function called: get_URL(" << host << ", " << path << ")\n";
+  // cerr << "Warning: get_URL() has not been implemented yet.\n";
 }
 
 int main( int argc, char* argv[] )
